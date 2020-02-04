3e5c3846b9e41d930462282a6c17bc41d88b433e
uttam <uttam@techelogy.com>
2020-02-04 12:27:43 +0530

    some ui and admin panel changes

---
+++ app/code/Techelogy/Backend/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE, 'Techelogy_Backend', __DIR__
+);
