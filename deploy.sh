a5ba86bd31d06d31b4c4888c9975b856450f6ba8
user05 <user05@node05.tact>
2020-03-02 11:56:37 +0530

    deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+
+
 __deployNow() {
 
 
@@ -7,24 +9,25 @@ __deployNow() {
     git pull origin master
 
 
+    #~ echo "### composer install ..." &&
+    #~ composer install
+
+
+
+sudo rm -rf var/di/* var/generation/* var/cache/* var/log/* var/page_cache/* var/session/* var/view_preprocessed/* pub/static/*
+sudo php bin/magento setup:upgrade
+sudo php bin/magento setup:di:compile
+sudo php bin/magento setup:static-content:deploy -f en_US
+sudo php bin/magento cache:clean
+sudo php bin/magento cache:flush
+sudo php bin/magento indexer:reindex
 
 
 
 
- rm -rf var/di/* var/generation/* var/cache/* var/log/* var/page_cache/* var/session/* var/view_preprocessed/* pub/static/*
-php bin/magento setup:upgrade
-php bin/magento setup:di:compile
-php bin/magento setup:static-content:deploy -f  en_US
-php bin/magento cache:clean
-php bin/magento cache:flush
-php bin/magento indexer:reindex
 
 
-
-
-
-
-    chmod -R 777 var generated pub/static pub/media
+    sudo chmod -R 777 var generated pub/static pub/media
 
     echo "*************************"
     echo "     Deployment done"
@@ -33,3 +36,5 @@ php bin/magento indexer:reindex
 
 
 __deployNow
+
+
