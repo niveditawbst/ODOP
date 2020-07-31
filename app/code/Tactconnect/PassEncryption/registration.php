527dd2327137e8a203cad3429c58f806b29e956d
user05 <user05@node05.tact>
2020-07-31 13:10:48 +0530

    security patches

---
+++ app/code/Tactconnect/PassEncryption/registration.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Tactconnect Magento 2 Module.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Magento Team of Tactconnect Group
+ * =================================================================
+ *                 MAGENTO EDITION USAGE NOTICE
+ * =================================================================
+ * This package designed for Magento 2 COMMUNITY edition
+ * Tactconnect Support does not guarantee correct work of this package
+ * on any other Magento edition except Magento 2 COMMUNITY edition.
+ * =================================================================
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Tactconnect_PassEncryption',
+    __DIR__
+);
