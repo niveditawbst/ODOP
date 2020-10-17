153c3d83ebe480d93ba369d0a399f629be0bf685
himanshutechelogy <himanshu@techelogy.com>
2020-10-17 20:16:27 +0530

    Disable payment method fix

--- app/code/Techelogy/Customization/Observer/DisablePaymentMethod.php
+++ app/code/Techelogy/Customization/Observer/DisablePaymentMethod.php
@@ -6,32 +6,51 @@ use Magento\Framework\App\RequestInterface;
 
 class DisablePaymentMethod implements ObserverInterface
 {
+	
+	/**
+     * RestrictPaymentMethodsObserver constructor.
+     * @param \Magento\Framework\App\Request\Http $request
+     * @param \Magento\Framework\ObjectManagerInterface $objectManager
+     * @param \Magento\Framework\App\State $state
+     */
+    public function __construct(
+        \Magento\Framework\ObjectManagerInterface $objectManager,
+        \Magento\Checkout\Model\Cart $cart
+    ){
+        $this->_objectManager = $objectManager;
+        $this->_cart = $cart;
+    }
 	public function execute(\Magento\Framework\Event\Observer $observer)
     {
-		$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+		//$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
 		
-        //~ echo '==='.$quoteObj = $observer->getQuote()->getId();exit;
-        //~ $quoteItems = $quoteObj->getAllItems();
-        $hasExhibitionProduct = true;
-        //~ foreach($quoteItems as $quoteItem){
-			//~ $productId = $quoteItem->getProductId();
-			//~ $productObj = $objectManager->create('Magento\Catalog\Model\ProductFactory')->create()->getCollection()
-							//~ ->addAttributeToSelect('exhibition_product')
-								//~ ->addAttributeToFilter('entity_id', $productId)
-									//~ ->getFirstItem();
-			//~ $exhibitionProduct = $productObj->getData('exhibition_product');
+        // echo '==='.$quoteObj = $observer->getQuote()->getId();exit;
+        $quoteObj = $this->_cart->getQuote();
+        if($quoteObj)
+        {
+
+	        $quoteItems = $quoteObj->getAllVisibleItems();
+	        $hasExhibitionProduct = false;
+	        foreach($quoteItems as $quoteItem){
+				$productId = $quoteItem->getProductId();
+				$productObj = $this->_objectManager->create('Magento\Catalog\Model\Product')->getCollection()
+								->addAttributeToSelect('exhibition_product')
+									->addAttributeToFilter('entity_id', $productId)
+										->getFirstItem();
+				$exhibitionProduct = $productObj->getData('exhibition_product');
+				
+				if($exhibitionProduct){
+					$hasExhibitionProduct = true;
+				}
+			}
 			
-			//~ if($exhibitionProduct){
-				//~ $hasExhibitionProduct = true;
-			//~ }
-		//~ }
-		
-		$paymentMethodCode = $observer->getEvent()->getMethodInstance()->getCode();
-        $disablePaymentList = ['banktransfer', 'cashondelivery'];
-        if($hasExhibitionProduct && in_array($paymentMethodCode, $disablePaymentList)){
-            $checkResult = $observer->getEvent()->getResult();
-            $checkResult->setData('is_available', false); //this is disabling the payment method at checkout page
-        }
+			$paymentMethodCode = $observer->getEvent()->getMethodInstance()->getCode();
+	        $disablePaymentList = ['banktransfer', 'cashondelivery'];
+	        if($hasExhibitionProduct && in_array($paymentMethodCode, $disablePaymentList)){
+	            $checkResult = $observer->getEvent()->getResult();
+	            $checkResult->setData('is_available', false); //this is disabling the payment method at checkout page
+	        }
+	    }
     }
 }
 ?>
